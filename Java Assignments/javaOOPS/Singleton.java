86cfa6ac0441ebaecc3169abe94b30ac4b8b929e
sdinkarr <sdinkarr@LIN25007657.corp.capgemini.com>
2021-11-01 09:48:42 +0530

    oops

---
+++ Java Assignments/javaOOPS/Singleton.java
@@ -0,0 +1,23 @@
+
+public class Singleton
+{
+	public static void main(String args[])
+	{
+		Abc obj1 = Abc.getInstance();
+	}
+}
+class Abc
+{
+	static Abc obj = new Abc(); 
+	private Abc()
+	{
+	
+	}
+	public static Abc getInstance()
+	{
+		return obj;
+	}	
+}
+
+
+		
