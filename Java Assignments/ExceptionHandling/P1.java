f94fcb9d1b50f236e69e805788eca8ed6f42fa6c
sdinkarr <sdinkarr@LIN25007657.corp.capgemini.com>
2021-11-02 23:10:56 +0530

    ex

--- Java Assignments/ExceptionHandling/P1.java
+++ Java Assignments/ExceptionHandling/P1.java
@@ -1,4 +1,16 @@
 
 public class P1 {
-
+public static void main(String args[]) {
+		try {
+		int n1 = Integer.parseInt(args[0]);
+		int n2 = Integer.parseInt(args[1]);
+		
+		int res = n1/n2;
+		System.out.println("res = " + res);
+		}
+	catch(ArithmeticException e) {
+		System.out.println(" you should not put n2 value 0");
+		}
+	}
 }
+	
