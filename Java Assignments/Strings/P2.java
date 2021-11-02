a62b29220cab0462c4382441149ef2fa1e0d094b
sdinkarr <sdinkarr@LIN25007657.corp.capgemini.com>
2021-11-02 17:51:44 +0530

    str

---
+++ Java Assignments/Strings/P2.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class P2 {
+		 public static void main(String[] args) {
+	
+		String str1 = "Hello Wolrd";
+		String str2 = " How Are You?";
+		String str = str1.concat(str2);
+		System.out.println(str);
+	
+}
+}
