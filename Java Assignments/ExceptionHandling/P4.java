f743b53a6cc56e093c30897d260b54e6f2119e8a
sdinkarr <sdinkarr@LIN25007657.corp.capgemini.com>
2021-11-06 16:40:54 +0530

    exc

---
+++ Java Assignments/ExceptionHandling/P4.java
@@ -0,0 +1,48 @@
+
+import java.util.Scanner;
+
+public class P4{
+	
+	static void fun() throws IllegalAccessException 
+    { 
+        double balance = 1000,  wamount;
+        
+        Scanner wd=new Scanner(System.in);
+        System.out.print("Withdraw an amount:");
+
+        wamount=wd.nextInt();
+        
+        balance -= wamount;
+        wd.close();
+
+
+        if (wamount<0)
+        {
+        throw new IllegalAccessException(); 
+        }
+        else if (balance<0)
+        {
+        	System.out.println("Insufficient Balance");
+        }
+        else {
+        	System.out.println("Your reamining balance is:"+balance);
+        	
+        }
+        
+        }
+
+
+
+    public static void main(String args[]) 
+    { 
+
+        try
+        { 
+            fun(); 
+        } 
+        catch(IllegalAccessException ex) 
+        { 
+            System.out.println("IllegalBankTransaction"); 
+        } 
+    } 
+} 
