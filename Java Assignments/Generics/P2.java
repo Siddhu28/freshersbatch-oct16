17738237fefe0a8b851299b17a0acb5e569f201e
sdinkarr <sdinkarr@LIN25007657.corp.capgemini.com>
2021-11-06 12:00:57 +0530

    gen

---
+++ Java Assignments/Generics/P2.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class P2 {
+
+	public static void main(String[] args) {
+		HashMap<Integer, Double> RandomNum = new HashMap<>();
+		
+		RandomNum.put(1, 28.67);
+		RandomNum.put(2, 82.77);
+		RandomNum.put(3, 34.09);
+		RandomNum.put(4, 67.11);
+		RandomNum.put(5, 55.53);
+		RandomNum.put(6, 63.87);
+		RandomNum.put(7, 99.99);
+		RandomNum.put(8, 91.05);
+		RandomNum.put(9, 18.97);
+		RandomNum.put(10, 26.7);
+		
+		System.out.println("Size of RndomNum is = " + RandomNum.size());
+		
+		System.out.println(RandomNum);
+		
+	}
+
+}
