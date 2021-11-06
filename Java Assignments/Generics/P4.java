752718f951eac6f6b6215ced918061d9b9f02f48
sdinkarr <sdinkarr@LIN25007657.corp.capgemini.com>
2021-11-06 13:06:35 +0530

    gen

---
+++ Java Assignments/Generics/P4.java
@@ -0,0 +1,30 @@
+
+public class Pair<k, v> {
+	
+	private k key;
+	private v value;
+	
+	public Pair(k key, v value) {
+		super();
+		this.key = key;
+		this.value = value;
+
+	}
+	
+	public< k, v> String showData() {
+		 return key + " " + value;
+	}
+}
+
+public class P4{
+	public static void main(String args[]) {
+		
+		Pair<String, String> obj1 = new Pair<String , String>("1", "Hello");
+		Pair<String, java.util.Date> obj2 = new Pair<>("Today is : ", new java.util.Date());
+		
+		System.out.println(obj1.showData());
+		System.out.println(obj2.showData());
+		
+		
+	}
+}
