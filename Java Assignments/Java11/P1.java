95067d50a4f134dd4b67ff2000c03b15e2b27e71
sdinkarr <sdinkarr@LIN25007657.corp.capgemini.com>
2021-11-07 20:12:55 +0530

    Java11

---
+++ Java Assignments/Java11/P1.java
@@ -0,0 +1,16 @@
+
+public class P1 {
+	
+	public static void main(String args[]) {
+		
+		var P=5000;
+		var R=7;
+		var T=5;
+		FunctionalInterface FuncObj = (var a,var b,var c) -> (a*b*c)/100;
+		System.out.println(FuncObj.SI_Interest(P, R, T));
+	}
+	
+	interface FunctionalInterface{
+		double SI_Interest(double P,double R,double T);
+	}
+}
